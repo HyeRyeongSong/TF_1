43b38d16d871e17c8ffa325696fbf77643f5d5d1
=HyeRyeongSong <=loverhdiddl@gmail.com>
2019-06-11 14:23:04 +0900

    1. placeholder를 통한 덧셈

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import tensorflow as tf
 
-node1 = tf.constant(3.0, tf.float32)
-node2 = tf.constant(4.0) #also tf.float32 implicitly
-node3 = tf.add(node1, node2) #node3 = node1 + node2
+#placeholder 노드 2개와 이를 더해주는 노드를 만들고 feed-dict를 통해 placeholder의 값을 넘겨받음
+#feed-dict: 이 그래프를 실행시키고 싶은데 placeholder 노드의 값을 모르겠으니 그 값들을 좀 넘겨줘
+#값을 넘겨주면서 그래프를 실행시키면 됨
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b # + provides a shortcut for tf.add(a, b)
 
-#그냥 출력하면 이것은 그래프의 하나의 요소다.
-#이런 형태의 Tensor야 라고만 말해주고 결과값이 나오진 않음
-#print("node1: ", node1, "node2: ", node2)
-#print("node2: ", node3)
-
-#결과값을 출력하기 위해서는 session을 만들고 여기에 내가 실행시키고 싶은 노드를 넣어줘야 함
+#session을 만들고 이 session을 실행시킴
 sess = tf.Session()
-print("sess.run(node1, node2): ", sess.run([node1, node2]))
-print("sess.run(node3): ", sess.run(node3))
+print(sess.run(adder_node, feed_dict = {a: 3, b: 4.5}))
+#array를 통해 placeholder에 여러개의 값들을 넘겨줄 수도 있음
+print(sess.run(adder_node, feed_dict = {a: [1, 3], b: [2, 4]}))
