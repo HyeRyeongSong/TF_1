29a1a9fc4322078006d6a675131eda563bb592d6
=HyeRyeongSong <=loverhdiddl@gmail.com>
2019-06-16 17:03:12 +0900

    3. tensorflow의 tf.train.GradientDescentOptimizer가 잘 작동하나(W=5 toW=1)

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ x_data = [1, 2, 3]
 y_data = [1, 2, 3]
 
 # W를 tensorflow variable로 선언하고 random 값을 줌
-W = tf.Variable(tf.random_normal([1]), name='weight')
+# W가 잘 내려가는지 확인하기 위해 말도 안되는 큰 값을 W에 넣고 시작
+W = tf.Variable(5.0)
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
@@ -18,17 +19,10 @@ hypothesis = X * W
 # tf.reduce_mean(t): tensor들의 평균을 내주는 함수
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
-# 수동으로 cost를 minimize하는 코드
-# 수동으로 구현한 Gradient descent 알고리즘
-# 수식을 그대로 써주면 미분을 이용한 Gradient descent 알고리즘을 tensorflow로 구현 가능
+# 우리가 복잡하게 선언했던 것을 이 함수를 통해 간단히 표현 가능
 # Minimize: Gradient Descent using derivative: W -= learning_rate * derivative
-learning_rate = 0.1
-gradient = tf.reduce_mean((W * X - Y) * X)
-descent = W - learning_rate * gradient
-# tensorflow는 '='으로 바로 assign 불가 (assign 함수를 통해 이뤄져야 함)
-# assign하는 operation을 update라는 노드에 할당해줌
-# 뒤에서 update 노드를 실행시키면 Graph 전체가 실행되며 위의 일련의 동작들이 일어남
-update = W.assign(descent)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+train = optimizer.minimize(cost)
 
 # Launch the graph in a session. (session을 만들고)
 sess = tf.Session()
@@ -36,8 +30,6 @@ sess = tf.Session()
 # 앞서 정의한 tensorflow variable인 W와 b를 사용하기 위해서는 이함수를 실행시켜서 variable들을 initialize해줘야 함
 sess.run(tf.global_variables_initializer())
 
-for step in range(21):
-    # update 노드를 실행시킴(Graph 실행)
-    sess.run(update, feed_dict={X: x_data, Y: y_data})
-    # 잘 실행이 되는 지 cost와 W 값 출력 (W가 1이 되야함)
-    print(step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W))
+for step in range(10):
+    print(step, sess.run(W))
+    sess.run(train, feed_dict={X: x_data, Y: y_data})
