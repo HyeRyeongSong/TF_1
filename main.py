de1d33861efee78373749db06ff57aef6779d3b3
=HyeRyeongSong <=loverhdiddl@gmail.com>
2019-06-16 16:32:28 +0900

    3. matplotlib 라이브러리를 통해 cost 함수가 어떤 모양인지 시각화

--- main.py
+++ main.py
@@ -1,37 +1,21 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-## placeholder를 사용하는 가장 큰 이유는 만들어진 모델에 대해서 값을 따로 넘길 수 있기 때문
-## Linear regression 모델을 만들어 놓은 후 학습 data를 따로 넘겨줄 수 있다
+# 학습 data X와 Y가 주어짐
+X = [1, 2, 3]
+Y = [1, 2, 3]
 
-# placeholders for a tensor that will be always fed using feed_dict
-# 학습 data X, Y의 값을 직접 주는 대신에 이렇게 placeholder로 줌 (shape도 지정 가능)
-# shape: 1차원 array이고 갯수는 원하는대로 알아서 (None이라는 것은 아무 값이나 들어올 수 있다는 의미)
-X = tf.placeholder(tf.float32, shape=[None])
-Y = tf.placeholder(tf.float32, shape=[None])
+# W를 placeholder로 줌
+# W의 값을 임의대로 바꿔가면서 그 값이 어떻게 되는지 보기위함
+W = tf.placeholder(tf.float32)
 
-# trainable한 tensorflow의 variable 선언
-# W와 b의 값을 모르니까 random한 값을 주게 됨
-# tensorflow의 shape을 정의하고 그 값을 준다
-# (값이 하나인 1차원 array(rank가 1)를 주게 됨)
-# tf.Variable: tensorflow가 사용하는 variable
-#              trainable variable(tensorflow가 학습하는 과정에서 자기가 변경시킨다)
-#              (tensorflow를 실행시키면 tensorflow 자체적으로 변경시키는 값)
-W = tf.Variable(tf.random_normal([1]), name = 'weight')
-b = tf.Variable(tf.random_normal([1]), name = 'bias')
-
-# Our hypothesis XW+b (hypothesis 정의)
-hypothesis = X * W + b
+# Our hypothesis for linear model X * W
+# 편의를 위해 hypothesis를 simple하게 줌
+hypothesis = X * W
 
 # cost/Losss function (cost 정의)
 # tf.reduce_mean(t): tensor들의 평균을 내주는 함수
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
-
-# Minimize(train할 때 cost를 minimize하라 해줌)
-# 1.optimizer를 GradientDescentOptimizer를 통해 정의하고
-# 2.optimizer의 minimize라는 함수를 호출하면서 무엇을 minimize할 지 넘겨줌
-# 3.optimizer가 우리가 앞서 정의한 tensorflow variable W와 b를 조정해서 자기가 스스로 minimize함
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-train = optimizer.minimize(cost)
 
 # Launch the graph in a session. (session을 만들고)
 sess = tf.Session()
@@ -39,25 +23,24 @@ sess = tf.Session()
 # 앞서 정의한 tensorflow variable인 W와 b를 사용하기 위해서는 이함수를 실행시켜서 variable들을 initialize해줘야 함
 sess.run(tf.global_variables_initializer())
 
-# Fit the line
-# 2000번 정도 step을 돌텐데 이때마다 출력하긴 힘이 드니까 한 20번에 한 번씩 출력해줌
+# Variables for plotting cost function
+# Graph를 실행시키면서 W의 값과 cost의 값을 저장할 list를 만들어줌
+W_val = []
+cost_val = []
 
-# train노드를 실행시켜야 cost를 minimize하고
-# (cost를 minimize) = tf.reduce_mean(tf.square(hypothesis - y_train))을 minimize
-# 이 hypothesis는 W와 b로 연결됨
+# range를 (-30 ~ 50)까지 이동시키면서
+for i in range(-30, 50):
+    # W를 (-3 ~ 5)까지 0.1 간격으로 작게 움직이겠다
+    feed_W = i * 0.1
+    # feed_dict를 통해 W에 변동된 값들을 전달함
+    curr_cost, curr_W = sess.run([cost, W], feed_dict={W: feed_W})
+    # W와 cost가 어떻게 변하는지를 curr_W와 curr_cost에 저장하고
+    # 이를 각각 cost_val, W_val 리스트에 넣어준다
+    W_val.append(curr_W)
+    cost_val.append(curr_cost)
 
-# train 노드를 실행 = 이 그래프를 따라 들어가서 결국은 W와 b에 어떤 값들을 저장할 수 있게
-# 다 연결이 되어있게 building이 된 Grapp를 실행한다는 의미
-
-for step  in range(2001): # train 노드를 실행(이때 학습이 일어남)
-    # session을 실행시킬 때 각각 실행해도되지만 list에 넣어서 한꺼번에 실행시켜도 됨
-    # train을 실행시킬 때 X와 Y의 값을 feed_dict를 통해 넘겨준다
-    cost_val, W_val, b_val, _ = sess.run([cost, W, b, train], feed_dict={X: [1, 2, 3, 4, 5], Y:[2.1, 3.1, 4.1, 5.1, 6.1]})
-    if step % 20 == 0:
-        print(step, cost_val, W_val, b_val) # 학습이 일어난 뒤에 cost, W, b의 값이 어떻게 되는지 관찰
-
-# Testing our model
-# hypothesis에 feed_dict로 X 값을 넘겨서 잘 예측하는지 확인해보자
-print(sess.run(hypothesis, feed_dict={X: [5]}))
-print(sess.run(hypothesis, feed_dict={X: [2.5]}))
-print(sess.run(hypothesis, feed_dict={X: [1.5, 3.5]}))
+# cost 함수가 어떤 모양인지 시각화를 해보자
+# Show the cost function
+# plt.plot(x축, y축)
+plt.plot(W_val, cost_val)
+plt.show()
